69ede95727d8c65a1adf184711a4794cb53669f2
Mykola.Fant <fantkolja@gmail.com>
2018-07-05 22:58:26 +0300

    update(tslint): add precommit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 // import * as express from 'express';
+// import { HttpServer } from './server/httpServer';
+
+// const server: HttpServer = {};
+
+console.log('Index.');
--- src/server/httpServer.ts
+++ src/server/httpServer.ts
@@ -2,5 +2,6 @@ import { RequestHandler } from 'express';
 
 export interface HttpServer {
   get(url: string, requestHandler: RequestHandler): void;
+  post(url: string, requestHandler: RequestHandler): void;
+  delete(url: string, requestHandler: RequestHandler): void;
 }
-
