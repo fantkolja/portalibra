bfcd90bc9a6be33dc31556ca3e152ef91230bcc0
Mykola.Fant <fantkolja@gmail.com>
2018-07-08 17:29:43 +0300

    update(tslint): setup tslint

--- src/api/server/index.ts
+++ src/api/server/index.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { HttpServer} from './httpServer.model';
+import { HttpServer } from './httpServer.model';
 
 export class ApiServer implements HttpServer {
   private app: express.Application;
