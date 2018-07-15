4a41a226d426d16cea03591256d3776fd80c403a
Mykola.Fant <fantkolja@gmail.com>
2018-07-15 13:35:17 +0300

    update(server): serve static frontend files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { ApiServer } from './api/server';
 import { DictionaryController } from './api/controllers/DictionaryController';
+import * as path from 'path';
+import * as express from 'express';
 
 const port: number = Number(process.env.API_PORT);
 const server: ApiServer = new ApiServer(port);
 
-server.addControllers([DictionaryController]);
+const staticRoot = path.join(__dirname, '../frontend/dist/frontend');
+const staticMiddleware: express.Handler = express.static(`${staticRoot}`);
 
+// app.use(logger('dev'));
+server.addMiddleware(staticMiddleware);
+
+server.addControllers([DictionaryController]);
 // TODO: useExpressServer(app, { // register created express server in routing-controllers
 //   controllers: [UserController] // and configure it the way you need (controllers, validation, etc.)
 // });
