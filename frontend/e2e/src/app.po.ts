86f90cad8fa2b675bd0df5232bfa2a83ddcda41b
Mykola.Fant <fantkolja@gmail.com>
2018-07-15 13:22:07 +0300

    update(frontend): configure project, fix linting

--- frontend/e2e/src/app.po.ts
+++ frontend/e2e/src/app.po.ts
@@ -1,11 +1,12 @@
 import { browser, by, element } from 'protractor';
+import { promise as wdpromise } from 'selenium-webdriver';
 
 export class AppPage {
-  navigateTo() {
+  public navigateTo(): wdpromise.Promise<any> {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  public getParagraphText(): any {
     return element(by.css('app-root h1')).getText();
   }
 }
