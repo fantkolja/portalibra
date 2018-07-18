b839cefcdcd850cb86c5f7a095e0b7edef9850e1
Mykola.Fant <fantkolja@gmail.com>
2018-07-18 22:51:01 +0300

    update(tutorial): routing middle (hero detail)

--- frontend/src/app/hero.service.ts
+++ frontend/src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-hero';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +10,13 @@ import { Observable, of } from 'rxjs';
 export class HeroService {
   private heroes: Hero[];
 
-  constructor() {
+  constructor(private messageService: MessageService) {
     this.heroes = HEROES;
   }
 
   public getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(this.heroes);
   }
 }
